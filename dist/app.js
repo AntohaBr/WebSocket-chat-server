abf4f5476f8fdef37e9d803757a2cefaf9cce63b
AntohaBr <brelanton@gmail.com>
2023-05-30 16:29:16 +0300

    add user name

--- dist/app.js
+++ dist/app.js
@@ -8,23 +8,43 @@ const http_1 = __importDefault(require("http"));
 const socket_io_1 = __importDefault(require("socket.io"));
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
-const io = (0, socket_io_1.default)(server);
-const messages = [
-    {
-        message: 'hello Ivan', id: 545878,
-        user: { id: 872317, name: 'Anton' }
-    },
-    {
-        message: 'hello Anton', id: 1231354,
-        user: { id: 55487, name: 'Ivan' }
-    }
-];
+const socket = (0, socket_io_1.default)(server);
 app.get('/', (req, res) => {
     res.send('Hello it`s WS server');
 });
-io.on('connection', (socketChannel) => {
+const messages = [
+    {
+        message: 'hello Ivan', id: '54q5878',
+        user: { id: '872i317', name: 'Anton' }
+    },
+    {
+        message: 'hello Anton', id: '1231f354',
+        user: { id: '55e487', name: 'Ivan' }
+    }
+];
+const usersState = new Map();
+socket.on('connection', (socketChannel) => {
+    usersState.set(socketChannel, { id: new Date().getTime().toString(), name: 'anonym' });
+    socket.on('disconnect', () => {
+        usersState.delete(socketChannel);
+    });
+    socketChannel.on('client-name-sent', (name) => {
+        if (typeof name !== 'string') {
+            return;
+        }
+        const user = usersState.get(socketChannel);
+        user.name = name;
+    });
     socketChannel.on('client-message-sent', (message) => {
-        console.log(message);
+        if (typeof message !== 'string') {
+            return;
+        }
+        const user = usersState.get(socketChannel);
+        let messageItem = {
+            message: message, id: new Date().getTime(),
+            user: { id: user.id, name: user.name }
+        };
+        socketChannel.emit('new-message-sent', messageItem);
     });
     socketChannel.emit('init-messages-published', messages);
     console.log('a user connected');
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,57 @@
-import express from "express";
-import http from "http";
-import socketIo from "socket.io";
+import express from 'express'
+import http from 'http'
+import socketIo from 'socket.io'
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
-const messages = [
-    {
-        message: 'hello Ivan', id: 545878,
-        user: {id: 872317, name: 'Anton'}
-    },
-    {
-        message: 'hello Anton', id: 1231354,
-        user: {id: 55487, name: 'Ivan'}
-    }
-]
+const socket = socketIo(server);
 
 app.get('/', (req, res) => {
     res.send('Hello it`s WS server');
 });
 
-io.on('connection', (socketChannel) => {
-    socketChannel.on('client-message-sent', (message: string) => {
-        console.log(message);
+const messages = [
+    {
+        message: 'hello Ivan', id: '54q5878',
+        user: {id: '872i317', name: 'Anton'}
+    },
+    {
+        message: 'hello Anton', id: '1231f354',
+        user: {id: '55e487', name: 'Ivan'}
+    }
+]
+
+const usersState = new Map()
+
+socket.on('connection', (socketChannel) => {
+    usersState.set(socketChannel, {id: new Date().getTime().toString(), name: 'anonym'})
+
+    socket.on('disconnect', () => {
+        usersState.delete(socketChannel)
+    })
+
+    socketChannel.on('client-name-sent', (name: string) => {
+        if (typeof name !== 'string') {
+            return
+        }
+
+        const user = usersState.get(socketChannel)
+        user.name = name
     });
+
+    socketChannel.on('client-message-sent', (message: string) => {
+        if (typeof message !== 'string') {
+            return
+        }
+        const user = usersState.get(socketChannel)
+
+        let messageItem = {
+            message: message, id: new Date().getTime(),
+            user: {id: user.id, name: user.name}
+        }
+        socketChannel.emit('new-message-sent', messageItem)
+    });
+
     socketChannel.emit('init-messages-published', messages)
     console.log('a user connected');
 });
